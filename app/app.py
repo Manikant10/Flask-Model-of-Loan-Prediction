4ad0299fa9a8c924f5736ee884675d8bc5eca5fc
Manikant10 <manikantraju123@gmail.com>
2025-01-05 21:40:37 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-app = Flask(__name__, template_folder='C:\\Users\\manic\\OneDrive\\Dokumen\\templates')
+app = Flask(__name__)
 
 import joblib
 import pandas as pd
